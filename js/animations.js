0da7f964d598f3f574e81c644a4135193701dfe4
Steven Nagie <sanagie@yahoo.com>
2016-11-03 18:58:35 -0600

    Working on the animation for the rules div. Tried jQuery and CSS, had trouble with both.

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-  
+
+  // $('#nav-icon').click(function() {
+  //   $('.nav-menu').slideToggle();
+  // });
+
+  // $(document).on('click', '#rules-link', function() {
+  //   $('#rules-container').css('display', 'block');
+  // });
+  //
+  // $(document).on('click', '.exit', function() {
+  //   $('#rules-container').css('display', 'none');
+  // });
+
+  // $(document).on('click', '#rules-link', function() {
+  //   $('.rules-container').addClass('rules-container-center');
+  // });
 
 });
