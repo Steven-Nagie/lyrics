11a4ca3c5cffdd0bc7431091149ab73a54bac631
Steven Nagie <sanagie@yahoo.com>
2016-11-04 11:31:47 -0600

    Having problems with Chrome responsive tool, gonna test it on mobile.

--- js/animations.js
+++ js/animations.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
   $(document).on('click', '#rules-link', function() {
     $('.rules-container').addClass('rules-container-center');
     // $('.rules-text').addClass('rules-text-center');
-    setTimeout(function() {$('.rules-text').addClass('rules-text-center');}, 1800);
+    setTimeout(function() {$('.rules-text').addClass('rules-text-center');}, 1100);
   });
 
   $(document).on('click', '.exit', function() {
